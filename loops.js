0bd038a929387c666e354a08ed6aa33ddd40470f
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-11-06 01:09:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
+<<<<<<< HEAD
     if (i === 1) {array.push(`I am 1 strange loop.`)
   } else { array.push(`I am ` +i+ ` strange loops.`)}
+=======
+    if (i === 1) {console.log("I am 1 strange loop.")
+    } else { console.log(" I am ${i} strange loops.")}
+>>>>>>> 7abd593b2f26e7ca0e934ae8ce678b25661efc1e
   }
   return array
 }
